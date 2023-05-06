36b346abd9be6c3d91a4039040ab408e1aa858ab
mambrocio <mikedbambrocio@gmail.com>
2023-05-05 21:22:25 -0400

    Managed to have one pass through connected

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,7 @@
-let charMaxLength = 128;
-let charMinLength = 8;
-let charLength;
-let defaultBox = '';
+/*Established values for the Passwords */
 
-
-let selectChar = [];
+const charMaxLength = 128; // Max Number of Characters Allowed
+const charMinLength = 8; // Min Number of Characters allowed
 let numbers = '0123456789';
 let charUpCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let charLowCases='abcdefghijklmnopqrstuvwxyz';
@@ -15,66 +12,88 @@ let charLimit = document.getElementById("charLimit");
 
 function numberGen(){
     checkboxNumb = document.getElementById("randNumb");
-    checkboxNumb.addEventListener('change', function(e) {
-    checkboxNumb.checked
-    console.log(checkboxNumb.checked)
+    checkboxNumb.addEventListener('change', function(e){
+    num = checkboxNumb
+    //console.log(checkboxNumb.checked)
+    return num;
 });
 }
 
 function upperGen(){
     checkboxUp = document.getElementById("randUpCase");
     checkboxUp.addEventListener('change', function(e) {
-    checkboxUp.checked
-    console.log(checkboxUp.checked)
+    up = checkboxUp
+    //console.log(checkboxUp.checked)
+    return up
 });
 }
 
 function lowerGen(){
     checkboxLow = document.getElementById("randLowCase");
     checkboxLow.addEventListener('change', function(e){
-    checkboxLow.checked
-    console.log(checkboxLow.checked);
+    low = checkboxLow
+    //console.log(checkboxLow.checked);
+    return low;
+
 });
 }
 
 function caseGen(){
     checkboxCases = document.getElementById("randCases");
     checkboxCases.addEventListener('change', function(e){
-    cases = checkboxCases.checked
+    cases = checkboxCases
     //console.log(checkboxCases.checked)
     return cases;
-})
+});
 }
 
-// numberGen()
-// upperGen()
-// lowerGen()
+numberGen()
+upperGen()
+lowerGen()
 caseGen()
 
 function generatePW(){
 
-    addCases = cases
-    console.log(addCases)
 
     if(charLimit.value > charMaxLength)
     alert("Password exceeds the character count of 128")
         else if (charLimit.value < charMinLength)
     alert("Please enter a password set value of at least 8")
     else{
-        console.log(charLimit.value)
-        charLimit.value 
+        let charLength = charLimit.value
+        console.log(charLength) 
     }
 
 
-    // let password = '';
-    // let charEnter;
-    // do{
-    //     if(checkboxCases.checked == true && upperGen == true && lowerGen == true && caseGen == true){
+    let digits = num.checked;
+    let tall = up.checked;
+    let short = low.checked;
+    let addCases = cases.checked;
+
+    try {
+        let digits = num.checked;
+        console.log(digits)
+
+        let tall = up.checked;
+        console.log(tall)
+
+        let short = low.checked;
+        console.log(short)
+
+        let addCases = cases.checked;
+        console.log(addCases)
+    } catch (e){
+        console.log(e)
+    }
 
 
-    // }
+    do{
+        if(digits === true && tall === true && short === true && addCases === true)
+            document.getElementById("printedPass").innerHTML = numbers + charUpCases + charLowCases + charCaseRandom;  
+            console.log("printedPass")
 
-    // } while (charLimit> charMinLength && charLimit < charMaxLength)
+
+    } while (charLimit > charMinLength && charLimit < charMaxLength)
 
 }
 
